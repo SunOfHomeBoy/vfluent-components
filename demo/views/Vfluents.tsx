22bcb047b5f5c6ea6eea6c6bc24173ca2b10731b
hjboss <hongjiangproject@gmail.com>
2018-06-13 18:46:55 +0800

    Initial commit

--- demo/views/Vfluents.tsx
+++ demo/views/Vfluents.tsx
@@ -1,12 +1,15 @@
 import Vue, { CreateElement } from 'vue'
 import { VueComponent, Component } from 'vue-component-decorator'
 import { theme } from '../config'
+import { Button } from '../../components'
 
 @Component
 export default class Vfluents extends VueComponent<any> {
         public render(h: CreateElement) {
                 return (
-                        <div class={theme.themePrefix + '-viewport'}>Hello, Vfluents ~ </div>
+                        <div class={theme.themePrefix + '-viewport'}>Hello, Vfluents ~
+                                <Button />
+                        </div>
                 )
         }
 }
