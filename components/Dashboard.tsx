a69b4c73efe1f7a997b6c3b597a7d6e857eed602
hjboss <hongjiangproject@gmail.com>
2018-06-14 18:22:51 +0800

    Initial commit

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -5,9 +5,30 @@ import utils from './utils'
 @Component
 export class Dashboard extends vfluents<any> {
         protected configures: {
+                id?: string // 組件ID 可空 默認值：空字符串
+                className?: string // 附加樣式 可空 默認值：空字符串
                 size?: string // 導航尺寸 可空 默認值：Default 可選值：Default | Small | Large | Huge
+                collapsed?: boolean // 側欄折疊 可空 默認值：FALSE
 
         } = {}
 
-        public render(h: CreateElement) { }
+        protected stateCollapsed: boolean
+
+        public render(h: CreateElement) {
+                return (
+                        <div id={this.configures.id} class={vfluents.cls([
+                                'pure-g',
+                                vfluents.themePrefix + 'dashboard',
+                                ['Small', 'Large', 'Huge'].indexOf(this.configures.size) !== -1
+                                        ? vfluents.themePrefix + 'dashboard-' + this.configures.size
+                                        : '',
+
+                        ])}>
+                        </div>
+                )
+        }
+
+        public created() {
+                this.stateCollapsed = this.stateCollapsed || this.configures.collapsed
+        }
 }
