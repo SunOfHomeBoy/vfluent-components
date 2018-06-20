44a838aafbd7565a3634f94675244beb77b9db50
hjboss <hongjiangproject@gmail.com>
2018-06-20 18:17:49 +0800

    Initial commit

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,6 +2,7 @@ import { Component, CreateElement, Props } from 'vue-component-decorator'
 import { vfluents } from './vfluents'
 import { Icon } from './Icon'
 import './styles/Button.scss'
+import utils from './utils';
 
 @Component
 export class Button extends vfluents {
@@ -43,7 +44,7 @@ export class Button extends vfluents {
                 let innerElement: any = this.$props.text
 
                 if (this.$props.icon) {
-                        let iconElement = <Icon name={this.$props.icon} size={this.$props.size === 'Small' ? 'Mini' : 'Small'} />
+                        let iconElement = <Icon name={this.$props.icon} size="Small" />
                         let textElement = this.$props.text
 
                         innerElement = this.$props.align === 'Bottom' || this.$props.align === 'Right'
@@ -56,21 +57,33 @@ export class Button extends vfluents {
                                 id={this.$props.id}
                                 class={vfluents.cls([
                                         'pure-button',
-                                        vfluents.themePrefix + 'btn',
+                                        vfluents.themePrefix + 'button',
                                         ['Primary', 'Secondary', 'Success', 'Danger', 'Warning', 'Info', 'Link'].indexOf(this.$props.type) !== -1
-                                                ? vfluents.themePrefix + 'btn-' + String(this.$props.type).toLowerCase()
+                                                ? vfluents.themePrefix + 'button-' + String(this.$props.type).toLowerCase()
+                                                : '',
+                                        ['Small', 'Large', 'Huge'].indexOf(this.$props.size) !== -1
+                                                ? vfluents.themePrefix + 'button-' + String(this.$props.size).toLowerCase()
+                                                : '',
+                                        ['Left', 'Right', 'Top', 'Bottom'].indexOf(this.$props.align) !== -1
+                                                ? vfluents.themePrefix + 'button-' + String(this.$props.align).toLowerCase()
+                                                : '',
+                                        this.$props.icon
+                                                ? vfluents.themePrefix + 'button-icon'
+                                                : '',
+                                        this.$props.icon && !this.$props.text
+                                                ? vfluents.themePrefix + 'button-icon-notext'
                                                 : '',
                                         this.$props.outline
-                                                ? vfluents.themePrefix + 'btn-outline'
+                                                ? vfluents.themePrefix + 'button-outline'
                                                 : '',
                                         this.$props.radius && !this.$props.circle
-                                                ? vfluents.themePrefix + 'btn-radius'
+                                                ? vfluents.themePrefix + 'button-radius'
                                                 : '',
                                         this.$props.circle && !this.$props.radius
-                                                ? vfluents.themePrefix + 'btn-circle'
+                                                ? vfluents.themePrefix + 'button-circle'
                                                 : '',
                                         this.$props.active && !this.$props.disabled
-                                                ? vfluents.themePrefix + 'btn-active'
+                                                ? vfluents.themePrefix + 'button-active'
                                                 : ''
                                 ])}
                                 style={{ width: this.$props.width }}
--- demo/views/ViewButton.tsx
+++ demo/views/ViewButton.tsx
@@ -15,14 +15,23 @@ export class ViewButton extends ViewComponent {
                         <article>
                                 <dfn>按鈕類型</dfn>
                                 <samp>
-                                        <Button width="10rem" type="Primary" text="Primary" />
-                                        <Button width="10rem" type="Secondary" text="Secondary" />
-                                        <Button width="10rem" type="Success" text="Success" />
-                                        <Button width="10rem" type="Danger" text="Danger" />
-                                        <Button width="10rem" type="Warning" text="Warning" />
-                                        <Button width="10rem" type="Info" text="Info" />
-                                        <Button width="10rem" type="Link" text="Link" />
-                                        <Button width="10rem" type="Default" text="Default" />
+                                        <Button width="15rem" type="Primary" text="Primary" />
+                                        <Button width="15rem" type="Secondary" text="Secondary" />
+                                        <Button width="15rem" type="Success" text="Success" />
+                                        <Button width="15rem" type="Danger" text="Danger" />
+                                        <Button width="15rem" type="Warning" text="Warning" />
+                                        <Button width="15rem" type="Info" text="Info" />
+                                        <Button width="15rem" type="Link" text="Link" />
+                                        <Button width="15rem" type="Default" text="Default" />
+                                        <br />
+                                        <Button width="15rem" type="Primary" text="Primary" icon="Github" />
+                                        <Button width="15rem" type="Secondary" text="Secondary" icon="Github" />
+                                        <Button width="15rem" type="Success" text="Success" icon="Github" />
+                                        <Button width="15rem" type="Danger" text="Danger" icon="Github" />
+                                        <Button width="15rem" type="Warning" text="Warning" icon="Github" />
+                                        <Button width="15rem" type="Info" text="Info" icon="Github" />
+                                        <Button width="15rem" type="Link" text="Link" icon="Github" />
+                                        <Button width="15rem" type="Default" text="Default" icon="Github" />
                                 </samp>
                                 <code>
                                         <p>
@@ -92,14 +101,23 @@ export class ViewButton extends ViewComponent {
                                 </code>
                                 <dfn>邊框樣式</dfn>
                                 <samp>
-                                        <Button width="10rem" outline={true} type="Primary" text="Primary" />
-                                        <Button width="10rem" outline={true} type="Secondary" text="Secondary" />
-                                        <Button width="10rem" outline={true} type="Success" text="Success" />
-                                        <Button width="10rem" outline={true} type="Danger" text="Danger" />
-                                        <Button width="10rem" outline={true} type="Warning" text="Warning" />
-                                        <Button width="10rem" outline={true} type="Info" text="Info" />
-                                        <Button width="10rem" outline={true} type="Link" text="Link" />
-                                        <Button width="10rem" outline={true} type="Default" text="Default" />
+                                        <Button width="15rem" outline={true} type="Primary" text="Primary" />
+                                        <Button width="15rem" outline={true} type="Secondary" text="Secondary" />
+                                        <Button width="15rem" outline={true} type="Success" text="Success" />
+                                        <Button width="15rem" outline={true} type="Danger" text="Danger" />
+                                        <Button width="15rem" outline={true} type="Warning" text="Warning" />
+                                        <Button width="15rem" outline={true} type="Info" text="Info" />
+                                        <Button width="15rem" outline={true} type="Link" text="Link" />
+                                        <Button width="15rem" outline={true} type="Default" text="Default" />
+                                        <br />
+                                        <Button width="15rem" outline={true} type="Primary" text="Primary" icon="Github" />
+                                        <Button width="15rem" outline={true} type="Secondary" text="Secondary" icon="Github" />
+                                        <Button width="15rem" outline={true} type="Success" text="Success" icon="Github" />
+                                        <Button width="15rem" outline={true} type="Danger" text="Danger" icon="Github" />
+                                        <Button width="15rem" outline={true} type="Warning" text="Warning" icon="Github" />
+                                        <Button width="15rem" outline={true} type="Info" text="Info" icon="Github" />
+                                        <Button width="15rem" outline={true} type="Link" text="Link" icon="Github" />
+                                        <Button width="15rem" outline={true} type="Default" text="Default" icon="Github" />
                                 </samp>
                                 <code>
                                         <p>
@@ -183,17 +201,72 @@ export class ViewButton extends ViewComponent {
                                                 <span class="tags">&nbsp;/&gt;</span>
                                         </p>
                                 </code>
+                                <dfn>按鈕尺寸</dfn>
+                                <samp>
+                                        <Button width="15rem" type="Primary" size="Small" text="Small" />
+                                        <Button width="15rem" type="Primary" size="Small" text="Small" icon="Github" />
+                                        <br />
+                                        <Button width="15rem" type="Primary" size="Default" text="Default" />
+                                        <Button width="15rem" type="Primary" size="Default" text="Default" icon="Github" />
+                                        <br />
+                                        <Button width="15rem" type="Primary" size="Large" text="Large" />
+                                        <Button width="15rem" type="Primary" size="Large" text="Large" icon="Github" />
+                                        <br />
+                                        <Button width="15rem" type="Primary" size="Huge" text="Huge" />
+                                        <Button width="15rem" type="Primary" size="Huge" text="Huge" icon="Github" />
+                                </samp>
+                                <code></code>
+                                <dfn>按鈕圖標</dfn>
+                                <samp>
+                                        <Button icon="Github" type="Primary" size="Small" text="Icon+Primary+Small" />
+                                        <Button icon="Github" type="Primary" size="Default" text="Icon+Primary+Default" />
+                                        <Button icon="Github" type="Primary" size="Large" text="Icon+Primary+Large" />
+                                        <Button icon="Github" type="Primary" size="Huge" text="Icon+Primary+Huge" />
+                                        <br />
+                                        <Button icon="Github" type="Primary" size="Small" />
+                                        <Button icon="Github" type="Primary" size="Default" />
+                                        <Button icon="Github" type="Primary" size="Large" />
+                                        <Button icon="Github" type="Primary" size="Huge" />
+                                        <br />
+                                        <Button icon="Github" type="Primary" size="Small" circle={true} />
+                                        <Button icon="Github" type="Primary" size="Default" circle={true} />
+                                        <Button icon="Github" type="Primary" size="Large" circle={true} />
+                                        <Button icon="Github" type="Primary" size="Huge" circle={true} />
+                                </samp>
+                                <dfn>圖標對齊</dfn>
+                                <samp>
+                                        <Button width="15rem" type="Primary" icon="Github" align="Left" size="Small" text="Left+Small" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Left" size="Default" text="Left+Default" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Left" size="Large" text="Left+Large" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Left" size="Huge" text="Left+Huge" />
+                                        <br />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Right" size="Small" text="Right+Small" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Right" size="Default" text="Right+Default" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Right" size="Large" text="Right+Large" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Right" size="Huge" text="Right+Huge" />
+                                        <br />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Top" size="Small" text="Top+Small" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Top" size="Default" text="Top+Default" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Top" size="Large" text="Top+Large" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Top" size="Huge" text="Top+Huge" />
+                                        <br />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Bottom" size="Small" text="Bottom+Small" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Bottom" size="Default" text="Bottom+Default" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Bottom" size="Large" text="Bottom+Large" />
+                                        <Button width="15rem" type="Primary" icon="Github" align="Bottom" size="Huge" text="Bottom+Huge" />
+                                </samp>
+                                <code></code>
                                 <dfn>圓形圓角</dfn>
                                 <samp>
                                         <Button type="Primary" circle={false} radius={false} icon="Github" text="Icon-Circle-Radius+Text" />
                                         <Button type="Primary" circle={false} radius={true} icon="Github" text="Icon-Circle+Radius+Text" />
                                         <Button type="Primary" circle={true} radius={false} icon="Github" text="Icon+Circle-Radius+Text" />
-                                        <Button type="Primary" circle={false} radius={false} icon="Github" text="Icon+Circle-Radius-Text" />
+                                        <Button type="Primary" circle={true} radius={false} icon="Github" />
                                 </samp>
-                                <dfn>Active狀態</dfn>
+                                <dfn>按鈕狀態</dfn>
                                 <samp>
-                                        <Button active={true} type="Primary" text="Active-Outlink" />
-                                        <Button active={true} type="Primary" text="Active+Outlink" outline={true} />
+                                        <Button width="15rem" active={true} type="Primary" text="Active-Outlink" />
+                                        <Button width="15rem" active={true} type="Primary" text="Active+Outlink" outline={true} />
                                 </samp>
                                 <code>
                                         <p>
@@ -215,7 +288,7 @@ export class ViewButton extends ViewComponent {
                                                 <span class="attr">text=</span>
                                                 <span class="info">"Active-Outlink"</span>
                                                 <span class="attr">outline=</span>
-                                                <span class="info">&#125;true&#125;</span>
+                                                <span class="info">&#123;true&#125;</span>
                                                 <span class="tags">&nbsp;/&gt;</span>
                                         </p>
                                 </code>
