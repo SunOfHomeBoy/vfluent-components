a5f3d66f7d1f22d7996c3cf0986a900ca1ced540
hjboss <hongjiangproject@gmail.com>
2018-06-27 18:36:32 +0800

    Initial commit

--- demo/views/ViewNavBar.tsx
+++ demo/views/ViewNavBar.tsx
@@ -74,7 +74,7 @@ export class ViewNavBar extends ViewComponent {
                                         <NavBar brandName="vFluent-Components" brandLogo="Github" items={this.navbarItems} size="Huge" />
                                 </samp>
                                 <code></code>
-                                <dfn>移動導航</dfn>
+                                <dfn>移動版導航</dfn>
                                 <samp>
                                         <p>文本標題</p>
                                         <NavBar>頁面標題</NavBar>
@@ -85,8 +85,17 @@ export class ViewNavBar extends ViewComponent {
                                         <p>文本標題+文本按鈕</p>
                                         <NavBar prepend={[{ text: '微博' }]} append={[{ text: '微信' }]}>頁面標圖</NavBar>
                                         <p>自定義組件</p>
-                                        <NavBar prepend={[{ text: '微博', icon: 'Weibo' }]} append={[{ icon: 'Weixin' }]}><Button type="Success" text="頁面標題" block={true} /></NavBar>
+                                        <NavBar prepend={[{ text: '微博', icon: 'Weibo' }]} append={[{ icon: 'Weixin' }]}>
+                                                <Button type="Success" text="頁面標題" block={true} />
+                                        </NavBar>
                                 </samp>
+                                <code></code>
+                                <dfn>工具欄導航</dfn>
+                                <samp></samp>
+                                <code></code>
+                                <dfn>響應式導航</dfn>
+                                <samp></samp>
+                                <code></code>
                         </article>
                 )
         }
